e0cc6ef0bbd71bd1fce5ddf4fffe4c263ddc9372
Rachel Sakry <rachelsakry@gmail.com>
2012-08-05 17:31:00 -0700

    Added current week data to goals index view

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,22 +4,28 @@ class Report < ActiveRecord::Base
   attr_accessible :goal_id, :goal_met, :report_date
   belongs_to :goal
 
-  def current_week(goal_key)
-  	Report.where("report_date >= :this_sunday 
+  def self.current_week(goal_key)
+  	reports = Report.where("report_date >= :this_sunday 
   		and report_date <= :this_saturday 
   		and goal_id == :goal_id", 
   		:this_sunday => current_sunday(), 
   		:this_saturday => current_saturday(),
   		:goal_id => goal_key)
+    bools = []
+    reports.each { |report| bools << report.goal_met }
+    bools
   end
 
-	def current_sunday
+	def self.current_sunday
 		today = Date.today
 		day_of_week = today.wday  # wday returns the day of week (0-6, Sunday is zero).
 		sunday = today - day_of_week
 	end
 
-	def current_saturday
-		#write this
+	def self.current_saturday
+		today = Date.today        # hmm,
+    day_of_week = today.wday  # this stuff's redundant.
+    diff = 6 - day_of_week
+    saturday = today + diff
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,19 @@ more_goals = [{:goal_name => "meditate", :times_per_week => 7},
 	{:goal_name => "be in bed by 11", :times_per_week => 5},
 	{:goal_name => "exercise", :times_per_week => 3}]
 
-Goal.send(:attr_accessible, :goal_name, :times_per_week)
+Goal.send(:attr_accessible, :goal_name, :times_per_week)  # is this line is necessary?
 
-more_goals.each do |goal|
-	Goal.create!(goal)
+# more_goals.each do |goal|
+# 	Goal.create!(goal)
+# end
+
+reports = [{:goal_id => 2, :goal_met => true, :report_date => "2012-08-05"},
+	{:goal_id => 3, :goal_met => true, :report_date => "2012-08-05"},
+	{:goal_id => 4, :goal_met => true, :report_date => "2012-08-05"}
+]
+
+Report.send(:attr_accessible, :goal_id, :goal_met, :report_date)
+
+reports.each do |report|
+	Report.create!(report)
 end
